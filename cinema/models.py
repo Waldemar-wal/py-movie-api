8e66a5e54dbf6441cde3949e38294071c5b6a5f4
Vova Savenkov <sava.v80@gmail.com>
2025-04-04 15:51:56 +0300

    Create api views

---
+++ cinema/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return self.title
